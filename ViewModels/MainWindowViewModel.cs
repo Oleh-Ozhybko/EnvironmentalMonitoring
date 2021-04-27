610dcea02bc1abc14d9c16b1b8813c0f0e8af6ff
Oleh-Ozhybko <olehozhybko@gmail.com>
2021-04-27 15:45:30 +0300

    Added test property ViewModel

---
+++ ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EnvironmentalMonitoring.ViewModels.Base;
+
+namespace EnvironmentalMonitoring.ViewModels
+{
+    class MainWindowViewModel : ViewModelBase
+    {
+        #region Title of the main window
+        private string _title = "Environmental Monitoring in the Lviv region";
+        ///<summary>Заголовок вікна</summary>
+        public string Title
+        {
+            get => _title;
+            set => Set(ref _title, value);
+        }
+        #endregion
+    }
+}
