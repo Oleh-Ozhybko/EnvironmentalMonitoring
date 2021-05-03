e99dda8342b9a10975de5c9ebcba59188291d833
Oleh-Ozhybko <olehozhybko@gmail.com>
2021-05-03 14:46:53 +0300

    Added ApplicationContext

---
+++ Data/ApplicationContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EnvironmentalMonitoring.Models;
+
+
+namespace EnvironmentalMonitoring.Data
+{
+    class ApplicationContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public ApplicationContext() { }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("DefaultSqlConnection");
+        }
+
+    }
+}
