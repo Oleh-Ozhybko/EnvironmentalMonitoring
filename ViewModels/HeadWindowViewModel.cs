7a1acf764b5cd93b3e39b1e75848470411ee5369
Oleh-Ozhybko <olehozhybko@gmail.com>
2021-05-07 17:43:05 +0300

    Added HeadWindowViewModel w/ the title property

---
+++ ViewModels/HeadWindowViewModel.cs
@@ -0,0 +1,34 @@
+﻿using EnvironmentalMonitoring.ViewModels.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnvironmentalMonitoring.ViewModels
+{
+    class HeadWindowViewModel : ViewModelBase
+    {
+        #region Properties
+        #region Title
+        private string _title = "Моніторинг Екологічного середовища у Львівській області";
+        public string Title
+        {
+            get => _title;
+            set => Set(ref _title, value);
+        }
+        #endregion
+        #endregion
+
+        #region Commands
+
+        #endregion
+
+        public HeadWindowViewModel()
+        {
+            #region Commands
+
+            #endregion
+        }
+    }
+}
